ff77b55e541fcb0f9f5373e7aa42035f28d6e035
Ron Sprenkels <ron.sprenkels@gmail.com>
2016-09-27 19:01:32 +0200

    need these too

---
+++ kattis/Kattis/src/bookingARoom/BookingARoomTest.java
@@ -0,0 +1,30 @@
+package bookingARoom;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+
+public class BookingARoomTest {
+	
+	@Test
+	public void test() throws IOException {
+		System.setIn(BookingARoom.class.getResourceAsStream("1.in"));
+	
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		
+		BookingARoom testObject= new BookingARoom();
+		testObject.processInput();
+		
+		String expected = IOUtils.toString(BookingARoom.class.getResourceAsStream("1.ans"), "UTF-8"); 
+		String result = baos.toString();
+		assertEquals(expected, result);
+	}
+}
