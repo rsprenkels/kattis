08b6a8087fcc22a33c213b4154600383284a11b1
Ron Sprenkels <ron.sprenkels@gmail.com>
2016-10-05 17:20:56 +0200

    ladder

--- kattis/Kattis/src/ladder/Ladder.java
+++ kattis/Kattis/src/ladder/Ladder.java
@@ -1,11 +1,7 @@
 package ladder;
 
 import java.io.BufferedInputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class Ladder {
