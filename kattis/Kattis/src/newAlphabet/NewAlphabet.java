900bdf99f231e20c2346d587d3193afb0a7dd73b
ron sprenkels <ron.sprenkels@gmail.com>
2016-10-12 10:15:42 +0200

    no google stuff here

--- kattis/Kattis/src/newAlphabet/NewAlphabet.java
+++ kattis/Kattis/src/newAlphabet/NewAlphabet.java
@@ -1,42 +1,43 @@
 package newAlphabet;
 
 import java.io.BufferedInputStream;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-import com.google.common.collect.ImmutableMap;
-
 public class NewAlphabet {
 
-	static final Map<Character, String> MY_MAP = ImmutableMap.<Character, String>builder()
-		    .put('a', "@")
-		    .put('b', "8")
-		    .put('c', "(")
-		    .put('d', "|)")
-		    .put('e', "3")
-		    .put('f', "#")
-		    .put('g', "6")
-		    .put('h', "[-]")
-		    .put('i', "|")
-		    .put('j', "_|")
-		    .put('k', "|<")
-		    .put('l', "1")
-		    .put('m', "[]\\/[]")
-		    .put('n', "[]\\[]")
-		    .put('o', "0")
-		    .put('p', "|D")
-		    .put('q', "(,)")
-		    .put('r', "|Z")
-		    .put('s', "$")
-		    .put('t', "']['")
-		    .put('u', "|_|")
-		    .put('v', "\\/")
-		    .put('w', "\\/\\/")
-		    .put('x', "}{")
-		    .put('y', "`/")
-		    .put('z', "2")
-		    .build();
-	
+	static Map<Character, String> myMap;
+	static {
+		myMap = new HashMap<Character, String>();
+		myMap.put('a', "@");
+	    myMap.put('b', "8");
+	    myMap.put('c', "(");
+	    myMap.put('d', "|)");
+	    myMap.put('e', "3");
+	    myMap.put('f', "#");
+	    myMap.put('g', "6");
+	    myMap.put('h', "[-]");
+	    myMap.put('i', "|");
+	    myMap.put('j', "_|");
+	    myMap.put('k', "|<");
+	    myMap.put('l', "1");
+	    myMap.put('m', "[]\\/[]");
+	    myMap.put('n', "[]\\[]");
+	    myMap.put('o', "0");
+	    myMap.put('p', "|D");
+	    myMap.put('q', "(,)");
+	    myMap.put('r', "|Z");
+	    myMap.put('s', "$");
+	    myMap.put('t', "']['");
+	    myMap.put('u', "|_|");
+	    myMap.put('v', "\\/");
+	    myMap.put('w', "\\/\\/");
+	    myMap.put('x', "}{");
+	    myMap.put('y', "`/");
+	    myMap.put('z', "2");
+	}
+		
 	public static void main(String [] args) {
 		Scanner scan = new Scanner(new BufferedInputStream(System.in));
 		
@@ -45,7 +46,7 @@ public class NewAlphabet {
 		
 		for (char c : plaintext.toCharArray()) {
 			if (Character.isAlphabetic(c)) {
-				result.append(MY_MAP.get(Character.toLowerCase(c)));
+				result.append(myMap.get(Character.toLowerCase(c)));
 			} else {
 				result.append(c);
 			}
