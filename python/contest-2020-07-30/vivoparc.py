c538b4bfba2ec805752b83bf596f25ae0360c65b
Ron Sprenkels <ron.sprenkels@gmail.com>
2020-07-30 23:43:15 +0200

    performance lacks

--- python/contest-2020-07-30/vivoparc.py
+++ python/contest-2020-07-30/vivoparc.py
@@ -1,3 +1,5 @@
+import sys
+import timeit
 from collections import defaultdict
 from typing import Sequence, Tuple, Set
 
@@ -6,11 +8,19 @@ def g2(n, sees, sofar = None):
     if sofar == None:
         sofar = {}
     if n > 1:
-        for x in range(1, 5):
+        nxt = len(sofar) + 1
+        nxt_seen_by = [k for k in sees if nxt in sees[k]]
+        d = [sofar[k] for k in nxt_seen_by if k in sofar]
+        opts = [x for x in range(1, 5) if x not in d]
+        for x in opts:
             yield from g2(n - 1, sees, {**sofar,  **{len(sofar) + 1 : x}})
     else:
-        for x in range(1, 5):
-            yield {**sofar,  **{len(sofar) + 1 : x}}
+        nxt = len(sofar) + 1
+        nxt_seen_by = [k for k in sees if nxt in sees[k]]
+        d = [sofar[k] for k in nxt_seen_by if k in sofar]
+        opts = [x for x in range(1, 5) if x not in d]
+        for x in opts:
+            yield list({**sofar,  **{len(sofar) + 1 : x}}.values())
 
 def vivoparc(n: int, restrictions: Sequence[Tuple[int, int]]) -> Sequence[int]:
     sees = defaultdict(set)
@@ -44,3 +54,11 @@ def test_1():
 7-8
 """[1:-1].split('\n')]
     assert vivoparc(n, restrictions) == [1,2,3,4,3,2,3,2]
+
+if __name__ == '__main__':
+    n = int(input())
+    restrictions = []
+    for line in sys.stdin:
+        restrictions.append(tuple(map(int, line.split('-'))))
+    for ndx, v in enumerate(vivoparc(n, restrictions), start=1):
+        print(f'{ndx} {v}')
